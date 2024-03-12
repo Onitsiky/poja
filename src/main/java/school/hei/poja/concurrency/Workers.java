bddc98c71b676823d3f2573e87a9d4c7fa7ae6e4
Mahefaa <hei.mahefa@gmail.com>
2024-03-12 13:21:40 +0300

    chore: name frontal and worker threads for a better debug

--- src/main/java/school/hei/poja/concurrency/Workers.java
+++ src/main/java/school/hei/poja/concurrency/Workers.java
@@ -4,26 +4,32 @@ import school.hei.poja.PojaGenerated;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
-import static java.util.concurrent.Executors.newFixedThreadPool;
+import static school.hei.poja.concurrency.ThreadRenamer.getRandomSubThreadNamePrefixFrom;
+import static school.hei.poja.concurrency.ThreadRenamer.renameThread;
+import static java.util.concurrent.Executors.newVirtualThreadPerTaskExecutor;
+import static java.lang.Thread.currentThread;
 
 @PojaGenerated
 @Component
 public class Workers<T> {
     private final ExecutorService executorService;
 
-    public Workers(@Value("${workers.thread.number:5}") int nbThreads) {
-        this.executorService = newFixedThreadPool(nbThreads);
+    public Workers() {
+        this.executorService = newVirtualThreadPerTaskExecutor();
     }
 
     @SneakyThrows
     public List<Future<T>> invokeAll(List<Callable<T>> callables) {
+        var parentThread = currentThread();
+        callables = callables.stream().map(c -> (Callable<T>) () -> {
+            renameThread(currentThread(), getRandomSubThreadNamePrefixFrom(parentThread));
+            return c.call();
+        }).toList();
         return executorService.invokeAll(callables);
     }
 }
